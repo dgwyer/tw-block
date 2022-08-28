2be9c937776f7eb9b066d7a61cb011dd44f6be37
David Gwyer <d.v.gwyer@gmail.com>
2022-08-28 15:01:18 +0100

    add placeholder title field

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const { join } = require('path');
 
 module.exports = {
   defaultValues: {
+    title: 'Placeholder Title',
     folderName: 'src',
     editorScript: 'file:./index.js',
     editorStyle: 'file:./index.css',
     style: 'file:./style-index.css',
-    npmDependencies: ['tailwind', 'autoprefixer', 'cssnano', 'postcss', 'postcss-cli', 'postcss-multiple-tailwind' ]
+    npmDevDependencies: ['tailwind', 'autoprefixer', 'cssnano', 'postcss', 'postcss-cli', 'postcss-multiple-tailwind' ]
   },
   // templatesPath: join(__dirname, 'templates/plugin'),
   pluginTemplatesPath: join(__dirname, 'templates/plugin'),
